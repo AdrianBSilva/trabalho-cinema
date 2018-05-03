95c594e9c8f9c257a7f1efdc05d94e234a2483a2
Adrian Boaventura Silva <104996@PROWAY.TREINA>
2018-05-03 17:12:54 -0300

    Adicionado a estrutura

--- TrabalhoSistemaCinema/TrabalhoSistemaCinema/Cinema.cs
+++ TrabalhoSistemaCinema/TrabalhoSistemaCinema/Cinema.cs
@@ -8,5 +8,29 @@ namespace TrabalhoSistemaCinema
 {
     class Cinema
     {
+        // Todos os intens
+        string[] nomeClientes = new string[100];
+        int[] idadeClientes = new int[100];
+        bool[] ingressoAntecipado = new bool[100];
+        double[] compraIngresso = new double[100];
+
+        public void CadastroClientes()
+        {
+            
+        }
+        public void CadastroFilmes()
+        {
+
+        }
+        public void Alimentacao()
+        {
+
+        }
+        public void Funcionario()
+        {
+
+        }
+
+       
     }
 }
