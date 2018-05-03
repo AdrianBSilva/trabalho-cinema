afc9945a14b21f706538aceadf4fc99d6751b438
Adrian Boaventura Silva <104996@PROWAY.TREINA>
2018-05-03 17:26:10 -0300

    Continuaçao da estrutura

--- TrabalhoSistemaCinema/TrabalhoSistemaCinema/Cinema.cs
+++ TrabalhoSistemaCinema/TrabalhoSistemaCinema/Cinema.cs
@@ -13,6 +13,11 @@ namespace TrabalhoSistemaCinema
         int[] idadeClientes = new int[100];
         bool[] ingressoAntecipado = new bool[100];
         double[] compraIngresso = new double[100];
+        string[] nomeFilme = new string[100];
+        double[] duracaoFilme = new double[100];
+        int[] classificaoFilme = new int[100];
+        string[] generoFilme = new string[100];
+ 
 
         public void CadastroClientes()
         {
