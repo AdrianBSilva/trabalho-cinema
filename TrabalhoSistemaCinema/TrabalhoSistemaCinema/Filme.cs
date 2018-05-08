fe4d171c866901b7afdd446672c64ae7333c09d3
LucasRodrigoK <lucasrkthd@gmail.com>
2018-05-08 13:44:22 -0300

    Alteração
    Correção

--- TrabalhoSistemaCinema/TrabalhoSistemaCinema/Filme.cs
+++ TrabalhoSistemaCinema/TrabalhoSistemaCinema/Filme.cs
@@ -119,16 +119,19 @@ namespace TrabalhoSistemaCinema
             {
                 for (int i = 0; i < atual; i++)
                 {
+                    Console.WriteLien("Nome do filme: {0}")
                     Console.WriteLine(
                         String.Format(
-                        @"\nNome do filme: {0} 
-                        \nDuração do filme: {1} 
-                        \nClassificação do filme: {2} 
-                        \nGênero do filme: ",
+                        @"Nome do filme: {0} 
+                        Duração do filme: {1} 
+                        Classificação do filme: {2} 
+                        Gênero do filme: {3}",
                         nomeFilme[i], duracaoFilme[i], classificaoFilme[i], generoFilme[i]
                         )
-                        );
-                }                
+                        );                    
+                }
+                Console.WriteLine("Pressione alguma tecla para continuar...");
+                Console.ReadKey();
             }            
         }
 
@@ -153,6 +156,8 @@ namespace TrabalhoSistemaCinema
                     )
                     );
                     achou = true;
+                    Console.WriteLine("Pressione alguma tecla para continuar...");
+                    Console.ReadKey();
                     break;
 
                 }
